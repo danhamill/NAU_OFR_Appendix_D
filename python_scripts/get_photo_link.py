ea87a5038b54f2ae8ff28ceffb2c534a0cbafbf7
danhamill <hamill.daniel@gmail.com>
2017-07-27 10:50:21 -0700

    added bs4 approach

--- python_scripts/get_photo_link.py
+++ python_scripts/get_photo_link.py
@@ -8,8 +8,8 @@ Created on Tue Jul 25 15:47:46 2017
 import pandas as pd
 import requests
 from StringIO import StringIO
-
-
+from bs4 import BeautifulSoup
+import time
 
 def build_cam_url(cam_name):
     base_url = 'http://grandcanyon.usgs.gov/photos/SandbarPhotos'
@@ -32,8 +32,36 @@ def get_cam_dates(cam_name):
     df['data'] = pd.to_datetime(df['data'].astype(str), format='%Y%m%d')
     return df
 
+def get_cam_dates_bs4(cam_name):
+    #build url to find data
+    url = build_cam_url(cam_name)
+    
+    #Fetch Data
+    t = requests.get(url)
+    
+    #Make Soup
+    soup = BeautifulSoup(t.content)
+    
+    #find a hrefs
+    thing = soup.find_all('a')
+    
+    #pares out dates
+    a = [link.attrs['href'] for link in thing]
+    a = a[1:]
+    a = [g[-21:-13] for g in a]
+    
+    df = pd.DataFrame({'dates':a})
+    df['dates'] = pd.to_datetime(df['dates'].astype(str), format='%Y%m%d')
+    return df
+
 
 if __name__ == '__main__':
+    start = time.clock()
+    df1 =get_cam_dates('RC0629R')
+    elapsed = (time.clock() - start)
+    print "Processing took ", elapsed , "seconds to analyse using requests"
     
-
-
+    start = time.clock()
+    df2 = get_cam_dates_bs4('RC0629R')
+    elapsed = (time.clock() - start)
+    print "Processing took ", elapsed , "seconds to analyse using beautiful soup"
